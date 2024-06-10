a66b11e367ee48f8b754834e039b7892ec030656
basuabhirup <basu.abhirup27@gmail.com>
2024-06-10 23:45:14 +0530

    Improve DB model to add user_groups and migrate DB

--- events/models.py
+++ events/models.py
@@ -16,6 +16,9 @@ class Event(models.Model):
   location = models.CharField(max_length=255)  # geolocations to be later
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
   organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+  host = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='hosted_events')  
+  group = models.ForeignKey('Group', on_delete=models.CASCADE, null=True, blank=True)
+
   capacity = models.PositiveIntegerField(blank=True, null=True)  # Optional
   image = models.ImageField(upload_to='events/', blank=True, null=True)  # Optional
 
@@ -30,3 +33,14 @@ class Registration(models.Model):
 
   class Meta:
     unique_together = ('user', 'event')  # Ensure a user can only register for an event once
+
+class Group(models.Model):
+  """Model for groups."""
+  name = models.CharField(max_length=255)
+  description = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  members = models.ManyToManyField(User, related_name='user_groups')
+
+
+  def __str__(self):
+    return self.name
